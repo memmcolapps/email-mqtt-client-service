3f2f7cb50b2698af7666dccfeda5417b9d530129
Habeeb Soliu <69279792+iliasHB@users.noreply.github.com>
2025-03-18 14:47:01 +0100

    Update EmailController.java

--- src/main/java/com/memmcol/emailmqttclientservice/controller/EmailController.java
+++ src/main/java/com/memmcol/emailmqttclientservice/controller/EmailController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*") // Allow all origins
 @RequestMapping("/api")
 public class EmailController {
     @Autowired
